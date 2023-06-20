15da3f4ddcd6fbfaa46e1a11e3ce360faf3e21e4
Nikolozi Chikhladze <ch1khladze@Nikolozis-MacBook-Pro.local>
2023-06-20 18:00:56 +0400

    Updates

--- component---src-pages-works-index-js-0f19e918b53c34f36070.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[447],{5026:function(e,t,a){a.r(t);var l=a(7294),n=a(1685),r=a(5552);t.default=e=>{let{location:t}=e;return l.createElement(l.Fragment,null,l.createElement(n.h,{location:t}),l.createElement(r.A,null))}}}]);
-//# sourceMappingURL=component---src-pages-works-index-js-0f19e918b53c34f36070.js.map
